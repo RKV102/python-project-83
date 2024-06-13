bfd775d44fd04e95acae72fe753ed131b942a661
RKV102 <roman.krilow@mail.ru>
2024-06-13 12:54:35 +0500

    Небольшой рефакторинг скрипта создания таблиц в БД

--- database.sql
+++ database.sql
@@ -1,16 +1,16 @@
+DROP TABLE IF EXIST urls;
+DROP TABLE IF EXIST url_checks;
 CREATE TABLE IF NOT EXISTS urls (
   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(50),
+  name VARCHAR(50) UNIQUE,
   created_at TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS url_checks (
   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  url_id BIGINT,
+  url_id BIGINT REFERENCES urls(id),
   status_code INT,
   h1 TEXT,
   title TEXT,
   description TEXT,
   created_at TIMESTAMP
 );
-DELETE FROM urls *;
-DELETE FROM url_checks *;
